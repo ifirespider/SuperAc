f12a75a3d8e942bd6c18a491e3c082c31ab14573
ifirespider <ifirespider@gmail.com>
2026-06-16 23:04:36 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 export default defineNuxtConfig({
   // Get all the pages, components, composables and plugins from the parent theme
-  extends: ['./woonuxt_base',
-            './my_theme' 
+  extends: ['./woonuxt_base' 
 			],
   components: [{ path: './components', pathPrefix: false }],
   /**
