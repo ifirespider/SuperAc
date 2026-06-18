053cd50d0583cf07460b5f55ec6ec38b869f4508
ifirespider <ifirespider@gmail.com>
2026-06-18 13:32:43 +0800

    Update index.vue
    修改首页，保持统一代码。

--- my_theme/app/pages/index.vue
+++ my_theme/app/pages/index.vue
@@ -22,15 +22,6 @@ useSeoMeta({
   <main>
     <HeroBanner />
 
-    <div class="container flex flex-wrap items-center justify-center my-16 text-center gap-x-8 gap-y-4 brand lg:justify-between">
-      <img src="/images/logoipsum-211.svg" alt="Brand 1" width="132" height="35" />
-      <img src="/images/logoipsum-221.svg" alt="Brand 2" width="119" height="30" />
-      <img src="/images/logoipsum-225.svg" alt="Brand 3" width="49" height="48" />
-      <img src="/images/logoipsum-280.svg" alt="Brand 4" width="78" height="30" />
-      <img src="/images/logoipsum-284.svg" alt="Brand 5" width="70" height="44" />
-      <img src="/images/logoipsum-215.svg" alt="Brand 6" width="132" height="40" />
-    </div>
-
     <section class="container my-16">
       <div class="flex items-end justify-between">
         <h2 class="text-lg font-semibold md:text-2xl">{{ $t('shop.shopByCategory') }}</h2>
@@ -72,7 +63,7 @@ useSeoMeta({
       </div>
     </section>
 
-    <section v-if="popularProducts" class="container my-16">
+<section v-if="popularProducts" class="container my-16">
       <div class="flex items-end justify-between">
         <h2 class="text-lg font-semibold md:text-2xl">{{ $t('shop.popularProducts') }}</h2>
         <NuxtLink class="font-medium text-primary-dark" to="/products">{{ $t('general.viewAll') }}</NuxtLink>
